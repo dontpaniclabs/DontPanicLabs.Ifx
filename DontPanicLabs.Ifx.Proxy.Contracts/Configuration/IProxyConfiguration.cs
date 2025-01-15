425627f791c78f5ec30f4cc5965641aa32e2aba8
Tony Wilsman <twilsman@dontpaniclabs.com>
2025-01-15 12:03:59 -0600

    Missed config updates

--- DontPanicLabs.Ifx.Proxy.Contracts/Configuration/IProxyConfiguration.cs
+++ DontPanicLabs.Ifx.Proxy.Contracts/Configuration/IProxyConfiguration.cs
@@ -5,6 +5,9 @@ namespace DontPanicLabs.Ifx.Proxy.Contracts.Configuration
         // This is false by default
         bool AutoDiscoverServices { get; }
 
+        // This is false by default
+        bool IsInterceptionEnabled { get; }
+
         // For this dictionary, the key is the Interface, and the value is the list of implementations
         // that could resolve for the key (interface).
         Dictionary<Type, Type[]> ServiceRegistrations { get; }
