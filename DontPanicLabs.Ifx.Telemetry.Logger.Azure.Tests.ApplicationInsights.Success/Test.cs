1b6c89068cf2b8e1958924f079b9433b5099dbcc
Pavel Patino <ppatino@dontpaniclabs.com>
2025-06-09 10:20:56 -0500

    Memory leak demo: add failing test showing OOM exception on many instantiations of `Logger`

--- DontPanicLabs.Ifx.Telemetry.Logger.Azure.Tests.ApplicationInsights.Success/Test.cs
+++ DontPanicLabs.Ifx.Telemetry.Logger.Azure.Tests.ApplicationInsights.Success/Test.cs
@@ -55,5 +55,38 @@ namespace DontPanicLabs.Ifx.Telemetry.Logger.Azure.Tests.ApplicationInsights.Suc
         {
             _logger!.Exception(new Exception("Unit Test Exception"), _customProperties!);
         }
+
+        /// <summary>
+        /// DO NOT MERGE: A "demo" test showing memory behavior of multiple instantiations of `Logger` to be used in an
+        /// "A / B" test showing a failing test (OOM exception) when `Logger` is not disposed. A commit will then
+        /// be added  showing no OOM occurs after we `.Dispose()` the logger.
+        /// </summary>
+        [TestMethod]
+        public void MultipleInstantiations_ShouldNotLeakResources()
+        {
+            // Force GC before baseline snapshot
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            long totalMemBefore = GC.GetTotalMemory(true);
+
+            for (int i = 0; i < 250000; i++)
+            {
+                // Instantiate `Logger` without `Dispose` to show memory leak
+                var logger = new Azure.ApplicationInsights.Logger();
+                // logger.Dispose();
+            }
+
+            // Force GC again; 5Mb memory growth is just used as a rough heuristic for "acceptable" memory growth; the
+            // "A/B" test should be convincing without needing GC to do anything specific (e.g. behave like we would
+            // ideally want it to for
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            long totalMemAfter = GC.GetTotalMemory(true);
+
+            long leak = (totalMemAfter - totalMemBefore);
+            long leakThreshold = 5 * 1024 * 1024;
+
+            Assert.IsTrue(leak < leakThreshold, $"Leaked too much memory: {leak}Mb");
+        }
     }
 }
